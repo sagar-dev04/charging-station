b93672afc1c27a58facf4b24ed7a0a11d8169968
Sagar Sabharwal <sagar.dev0410@gmail.com>
2025-07-01 10:31:39 +0530

    react-compiler added

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,42 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react({
+      jsxRuntime: "automatic",
+      jsxImportSource: "react",
+      babel: {
+        plugins: [["babel-plugin-react-compiler", {}]],
+      },
+    }),
+  ],
+  build: {
+    sourcemap: false,
+    target: ["esnext"],
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          mui: ["@mui/joy", "@mui/system", "@emotion/react", "@emotion/styled"],
+        },
+        assetFileNames: (assetInfo) => {
+          if (!assetInfo.name) return "assets/[name]-[hash][extname]";
+
+          const extType = assetInfo.name.split(".").at(1) ?? "";
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            return `assets/images/[name]-[hash][extname]`;
+          }
+          if (/css/i.test(extType)) {
+            return "assets/css/[name]-[hash][extname]";
+          }
+          return `assets/[name]-[hash][extname]`;
+        },
+        chunkFileNames: "assets/js/[name]-[hash].js",
+        entryFileNames: "assets/js/[name]-[hash].js",
+      },
+    },
+  },
+});
