c01bfb6d94b98934546f7be4a9e03af54e1766c7
Sagar Sabharwal <sagar.dev0410@gmail.com>
2025-07-01 13:44:18 +0530

    firebase config env configuration

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCZCJXp6usC2hLsbkRatA4LKzBDNle7ghg",
-  authDomain: "sagar-dev-auth-app.firebaseapp.com",
-  projectId: "sagar-dev-auth-app",
-  storageBucket: "sagar-dev-auth-app.firebasestorage.app",
-  messagingSenderId: "561437230661",
-  appId: "1:561437230661:web:dc8127ecb64ac75c79c52c",
-  measurementId: "G-LEXMQF31N2"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
